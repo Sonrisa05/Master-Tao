ae1af2c81d88306fdccadcbffdfaab068cd04c64
Kateryna Mokhnach <katemohnach@gmail.com>
2023-11-17 11:41:43 +0100

    text

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,4 +1,15 @@
+$(document).ready(function () {
+    $('.first-screen-slider').slick({
+        dots: true,
+        appendArrows:'.first-screen-nav',
+        prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+    });
+})
+
+
 const burger = document.querySelector ('.hamburger');
 burger.addEventListener ('click', function() {
     burger.classList.toggle('is-active')
 })
+
